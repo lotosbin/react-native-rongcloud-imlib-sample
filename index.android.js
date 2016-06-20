c40d1b3010a881c78208dd7d58811f0d8425a504
liubinbin <lotosbin@gmail.com>
2016-06-20 14:03:01 +0800

    add samples

--- index.android.js
+++ index.android.js
@@ -6,13 +6,29 @@
 
 import React, { Component } from 'react';
 import {
+  NativeModules,
+  TouchableHighlight,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-
+import RongCloud from 'react-native-rongcloud-imlib'
 class sample extends Component {
+  _onPressInitWithKey(){
+    RongCloud.initWithAppKey('8luwapkvu3bbl');
+  }
+  _onPressConnect(){
+    RongCloud.connectWithToken('mPNDiKGpZlybEcWWrrQLT34jZh3+cDN8iZJS89BmLHSxeV2vGefG93bnXmX9jtFQAllnoWFRVbSKyoYSdfd52g==');
+  }
+  _onPressReceive(){
+    RongCloud.onReceived(()=> {
+      console.warn("received");
+    });
+  }
+  _onPressSendMessage(){
+    RongCloud.sendTextMessage(RongCloud.ConversationType.PRIVATE,"1","hello");
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -25,6 +41,10 @@ class sample extends Component {
         <Text style={styles.instructions}>
           Shake or press menu button for dev menu
         </Text>
+        <TouchableHighlight onPress={this._onPressInitWithKey}><Text>initWithKey</Text></TouchableHighlight>
+        <TouchableHighlight onPress={this._onPressConnect}><Text>Connect</Text></TouchableHighlight>
+        <TouchableHighlight onPress={this._onPressReceive}><Text>Receive</Text></TouchableHighlight>
+        <TouchableHighlight onPress={this._onPressSendMessage}><Text>SendMessage</Text></TouchableHighlight>
       </View>
     );
   }
