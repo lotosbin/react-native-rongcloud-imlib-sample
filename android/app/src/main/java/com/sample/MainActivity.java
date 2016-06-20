08e7e5af5b74eb527f8cbf363aea2a93e6df7c52
liubinbin <lotosbin@gmail.com>
2016-06-20 14:02:36 +0800

    rnpm link react-native-rongcloud-imlib

--- android/app/src/main/java/com/sample/MainActivity.java
+++ android/app/src/main/java/com/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sample;
 
 import com.facebook.react.ReactActivity;
+import org.lovebing.reactnative.rongcloud.RongCloudPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +35,8 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new RongCloudPackage()
         );
     }
 }
